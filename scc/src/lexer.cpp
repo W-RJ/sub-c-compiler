10ac98967c336ec5a576d56b88c8f9155510e63e
W-RJ <wang-rj@outlook.com>
2020-12-03 12:25:39 +0800

    feat: add class Lexer
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- scc/src/lexer.cpp
+++ scc/src/lexer.cpp
@@ -1,6 +1,34 @@
+#include <cstdio>
+
 #include "lexer.h"
 
 namespace scc
 {
+    // class Lexer
+    Lexer::Lexer() : fp(nullptr)
+    {
+    }
 
+    Lexer::~Lexer()
+    {
+        if (fp != nullptr)
+        {
+            fclose(fp);
+        }
+    }
+
+    bool Lexer::open(const char *fileName)
+    {
+        fp = fopen(fileName, "r, ccs=utf-8");
+        return fp != nullptr;
+    }
+
+    void Lexer::close()
+    {
+        if (fp != nullptr)
+        {
+            fclose(fp);
+            fp = nullptr;
+        }
+    }
 }
--- scc/src/lexer.h
+++ scc/src/lexer.h
@@ -3,9 +3,56 @@
 #ifndef _SCC_LEXER_H_
 #define _SCC_LEXER_H_
 
+#include <cstdio>
+
 namespace scc
 {
+    enum WordType
+    {
+        #include "sc.lang"
+    };
 
+    struct Word
+    {
+        WordType type;
+        const char *val;
+    };
+
+    class Lexer
+    {
+    private:
+
+        // File Pointer
+        FILE *fp;
+
+    public:
+
+        Lexer();
+
+        virtual ~Lexer();
+
+        /**
+         * Open source file
+         * 
+         * @param filename: name of source file
+         * 
+         * @return true if succeed, false otherwise
+         */
+        bool open(const char *fileName);
+
+        /**
+         * Close source file
+         */
+        void close();
+
+        /**
+         * Get next word
+         * 
+         * @return next word. type == NONE if fail
+         */
+        virtual Word nextWord() = 0; // NOTE
+
+    };
 }
 
 #endif // _SCC_LEXER_H_
