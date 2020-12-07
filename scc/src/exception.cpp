7543f1e1956e422f37984403aabed9d8997dc38f
W-RJ <wang-rj@outlook.com>
2020-12-07 15:27:13 +0800

    feat: add print() to exceptions
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- scc/src/exception.cpp
+++ scc/src/exception.cpp
@@ -2,6 +2,14 @@
 
 #include "exception.h"
 
+// class WRuntimeError
+
+const wchar_t* WRuntimeError::CMD_NAME = L"scc: ";
+
+const wchar_t* WRuntimeError::ERROR_PREFIX = L"error: ";
+
+const wchar_t* WRuntimeError::FATAL_ERROR_PREFIX = L"fatal error: ";
+
 WRuntimeError::WRuntimeError(const wchar_t *what_arg) : runtime_error(""), msg(what_arg)
 {
 }
@@ -14,4 +22,38 @@ const wchar_t* WRuntimeError::wwhat() const noexcept
 const char* WRuntimeError::what() const noexcept
 {
     return nullptr;
+}
+
+void WRuntimeError::print(FILE* fp) const noexcept
+{
+    fwprintf(fp, L"%ls%ls%ls\n", CMD_NAME, ERROR_PREFIX, msg);
+}
+
+// class NoSuchFileError
+
+const wchar_t* NoSuchFileError::MSG = L"No such file";
+
+NoSuchFileError::NoSuchFileError(const char* fileName, const wchar_t* fileType) :
+        WRuntimeError(fileType), fileName(fileName), fileType(fileType)
+{
+}
+
+void NoSuchFileError::print(FILE* fp) const noexcept
+{
+    fwprintf(fp, L"%ls%ls%s: %ls\n", CMD_NAME, ERROR_PREFIX, fileName, MSG);
+    fwprintf(fp, L"%ls%lsUnable to open %ls files\n", CMD_NAME, FATAL_ERROR_PREFIX, fileType);
+}
+
+// class InvalidArgumentError
+
+void InvalidArgumentError::print(FILE* fp) const noexcept
+{
+    fwprintf(fp, L"%ls%ls%ls\n", CMD_NAME, FATAL_ERROR_PREFIX, msg); // TODO
+}
+
+// class RegExpError
+
+void RegExpError::print(FILE* fp) const noexcept
+{
+    fwprintf(fp, L"%ls%ls In the definition of '%ls': %ls\n", CMD_NAME, ERROR_PREFIX, typeName, msg);
 }
--- scc/src/exception.h
+++ scc/src/exception.h
@@ -11,6 +11,12 @@ protected:
 
     const wchar_t *msg;
 
+    static const wchar_t* CMD_NAME;
+
+    static const wchar_t* ERROR_PREFIX;
+
+    static const wchar_t* FATAL_ERROR_PREFIX;
+
 public:
 
     explicit WRuntimeError(const wchar_t *what_arg);
@@ -26,16 +32,64 @@ public:
      * @deprecated use wwhat instead
      */
     virtual const char* what() const noexcept override;
+
+    /**
+     * print error message
+     */
+    virtual void print(FILE* fp) const noexcept;
 };
 
 class NoSuchFileError : public WRuntimeError
 {
-    using WRuntimeError::WRuntimeError;
+private:
+
+    const char* fileName;
+
+    const wchar_t* fileType;
+
+    static const wchar_t* MSG;
+
+public:
+    
+    NoSuchFileError(const char* fileName, const wchar_t* fileType);
+
+    /**
+     * print error message
+     */
+    virtual void print(FILE* fp) const noexcept;
 };
 
 class InvalidArgumentError : public WRuntimeError
 {
+public:
+
     using WRuntimeError::WRuntimeError;
+
+    /**
+     * print error message
+     */
+    virtual void print(FILE* fp) const noexcept;
+};
+
+class RegExpError : public WRuntimeError
+{
+private:
+
+    const wchar_t* typeName;
+
+public:
+
+    using WRuntimeError::WRuntimeError;
+
+    void setTypeName(const wchar_t* typeName)
+    {
+        this->typeName = typeName;
+    }
+
+    /**
+     * print error message
+     */
+    virtual void print(FILE* fp) const noexcept;
 };
 
 #endif // _SCC_EXCEPTION_H_
