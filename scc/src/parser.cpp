0a282a74b980e4cf80589cca81459f0fa32a3204
W-RJ <wang-rj@outlook.com>
2020-12-08 21:06:17 +0800

    feat: add Parser
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- scc/src/parser.cpp
+++ scc/src/parser.cpp
@@ -1,6 +1,26 @@
+#include "lexer.h"
 #include "parser.h"
+#include "exception.h"
 
 namespace scc
 {
+    // class Parser
 
+    Parser::Parser() : lexer(nullptr)
+    {
+    }
+
+    Parser::~Parser()
+    {
+        // TODO
+    }
+
+    void Parser::setLexer(Lexer* lexer)
+    {
+        if (lexer == nullptr)
+        {
+            throw NullPointerError(L"setLexer: lexer is null");
+        }
+        this->lexer = lexer;
+    }
 }
--- scc/src/parser.h
+++ scc/src/parser.h
@@ -3,9 +3,26 @@
 #ifndef _SCC_PARSER_H_
 #define _SCC_PARSER_H_
 
+#include "lexer.h"
+
 namespace scc
 {
+    class Parser
+    {
+    private:
 
+        Lexer* lexer;
+
+    public:
+
+        Parser();
+
+        virtual ~Parser();
+
+        void setLexer(Lexer* lexer);
+
+        virtual void parse() = 0;
+    };
 }
 
 #endif // _SCC_PARSER_H_
