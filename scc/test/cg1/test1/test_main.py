077719e838bdec113e19b0a991ea44174ae26765
W-RJ <wang-rj@outlook.com>
2020-12-16 00:33:30 +0800

    feat: modify tests
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- scc/test/cg1/test1/test_main.py
+++ scc/test/cg1/test1/test_main.py
@@ -7,6 +7,8 @@ output_dir = 'output'
 def test_main(tmpdir):
     scc = os.environ['SCC']
     shutil.copytree(os.path.join(os.path.dirname(__file__), input_dir), tmpdir.join(input_dir))
+    shutil.copy(scc, tmpdir.join(input_dir))
     os.chdir(tmpdir.join(input_dir))
-    os.system(scc + " > cout.txt")
+    os.system("./scc > cout.txt")
+    os.system("rm scc sc.lang testfile.txt")
     assert os.system('diff "' + str(os.path.join(os.path.dirname(__file__), output_dir)) + '" "' + str(tmpdir.join(input_dir)) + '"') == 0
