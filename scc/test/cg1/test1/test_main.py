9bc7567cff11ede3e04a8b7e2135a680ed413c12
W-RJ <wang-rj@outlook.com>
2020-12-17 13:22:47 +0800

    feat: add timeout to tests
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- scc/test/cg1/test1/test_main.py
+++ scc/test/cg1/test1/test_main.py
@@ -9,6 +9,6 @@ def test_main(tmpdir):
     shutil.copytree(os.path.join(os.path.dirname(__file__), input_dir), tmpdir.join(input_dir))
     shutil.copy(scc, tmpdir.join(input_dir))
     os.chdir(tmpdir.join(input_dir))
-    os.system("./scc > cout.txt")
+    os.system("timeout 1 ./scc > cout.txt")
     os.system("rm scc sc.lang testfile.txt")
     assert os.system('diff "' + str(os.path.join(os.path.dirname(__file__), output_dir)) + '" "' + str(tmpdir.join(input_dir)) + '"') == 0
