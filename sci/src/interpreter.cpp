cef260aa6a753e61f0ae7ab9ba3505b0f8c42703
W-RJ <wang-rj@outlook.com>
2020-12-21 12:41:40 +0800

    feat: add BInterpreter
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- sci/src/interpreter.cpp
+++ sci/src/interpreter.cpp
@@ -1,6 +1,55 @@
+#include <cstdio>
+#include <cstring>
+
+#include "../../common/src/bpcode.h"
+#include "../../common/src/exception.h"
 #include "interpreter.h"
 
 namespace sci
 {
+
+    // class BInterpreter
+
+    BInterpreter::BInterpreter() : codes(nullptr), del(false)
+    {
+    }
+
+    void BInterpreter::set(BPcode* codes)
+    {
+        this->codes = codes;
+        del = false;
+    }
+
+    void BInterpreter::read(const char* fileName)
+    {
+        FILE* fp;
+        if (strcmp(fileName, "-") == 0)
+        {
+            fp = stdin;
+        }
+        else
+        {
+            fp = fopen(fileName, "r");
+        }
+        if (fp == nullptr)
+        {
+            throw FileError(fileName, "input");
+        }
+        del = true;
+        // TODO
+    }
+
+    void BInterpreter::run()
+    {
+        // TODO
+    }
+
+    BInterpreter::~BInterpreter()
+    {
+        if (del)
+        {
+            delete[] codes;
+        }
+    }
 
 } // namespace sci
