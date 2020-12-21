f24783e4e205f302d882a2b323627e14ad4d692e
W-RJ <wang-rj@outlook.com>
2020-12-21 12:39:11 +0800

    feat: add Interpreters
    Signed-off-by: W-RJ <wang-rj@outlook.com>

---
+++ common/src/bpcode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef _SCI_BPCODE_H_
+#define _SCI_BPCODE_H_
+
+namespace sci
+{
+
+    struct BPcode
+    {
+        int f;
+        int a;
+    };
+
+} // namespace sci
+
+
+#endif // _SCI_BPCODE_H_
--- sci/src/interpreter.h
+++ sci/src/interpreter.h
@@ -3,10 +3,73 @@
 #ifndef _SCI_INTERPRETER_H_
 #define _SCI_INTERPRETER_H_
 
+#include <cstdint>
+#include <vector>
+
+#include "../../common/src/bpcode.h"
+
 namespace sci
 {
+    class Interpreter
+    {
+    public:
+
+        virtual void read(const char* fileName) = 0;
+
+        virtual void run() = 0;
+
+        virtual ~Interpreter() = default;
+    };
+
+    class BInterpreter : public Interpreter
+    {
+    private:
+
+        BPcode* codes;
+
+        bool del;
+
+    public:
+
+        BInterpreter();
+
+        void set(BPcode* codes);
+
+        virtual void read(const char* fileName) override;
+
+        virtual void run() override;
+
+        virtual ~BInterpreter() override;
+    };
+
+    class TInterpreter : public Interpreter
+    {
+    private:
+
+        union F
+        {
+            char name[4];
+            int32_t id;
+        };
+
+        struct TPcode
+        {
+            F f;
+            int l;
+            int a;
+        };
+
+        std::vector<TPcode> codes;
+
+    public:
+
+        TInterpreter();
+
+        virtual void read(const char* fileName) override;
+
+        virtual void run() override;
+    };
 
 } // namespace sci
-
 
 #endif // _SCI_INTERPRETER_H_
