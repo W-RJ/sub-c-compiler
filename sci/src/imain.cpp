0c3a98f295bf9f598e8dbba92eb54242aa7106c5
W-RJ <wang-rj@outlook.com>
2020-12-24 11:41:02 +0800

    feat: add runBin to imain
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- sci/src/imain.cpp
+++ sci/src/imain.cpp
@@ -1,5 +1,14 @@
 #include "interpreter.h"
 
+void runBin(const char* fileName)
+{
+    sci::BInterpreter interpreter;
+
+    interpreter.read(fileName);
+
+    interpreter.run();
+}
+
 int imain(int argc, char** argv)
 {
 
