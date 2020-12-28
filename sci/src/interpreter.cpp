e8d85c1ff66718842f4af5afef4fba4434eb985b
W-RJ <wang-rj@outlook.com>
2020-12-29 00:06:40 +0800

    feat: add run to TInterpreter
    Signed-off-by: W-RJ <wang-rj@outlook.com>

--- sci/src/interpreter.cpp
+++ sci/src/interpreter.cpp
@@ -399,6 +399,258 @@ namespace sci
 
     void TInterpreter::run()
     {
+        while (true)
+        {
+            switch (codes[++ip].f.id)
+            {
+            case 0x504f50: // POP
+                top -= codes[ip].a;
+                continue;
+
+            case 0x54494c: // LIT
+                if (++top < static_cast<int>(st.size()))
+                {
+                    st[top] = codes[ip].a;
+                    continue;
+                }
+                else
+                {
+                    st.push_back(codes[ip].a);
+                    continue;
+                }
+
+            case 0x444f4c: // LOD
+                if (codes[ip].l == 0)
+                {
+                    if (++top < static_cast<int>(st.size()))
+                    {
+                        st[top] = st[sp + codes[ip].a];
+                        continue;
+                    }
+                    else
+                    {
+                        st.push_back(st[sp + codes[ip].a]);
+                        continue;
+                    }
+                }
+                else
+                {
+                    if (++top < static_cast<int>(st.size()))
+                    {
+                        st[top] = st[codes[ip].a];
+                        continue;
+                    }
+                    else
+                    {
+                        st.push_back(st[codes[ip].a]);
+                        continue;
+                    }
+                }
+
+            case 0x4f5453: // STO
+                switch (codes[ip].l)
+                {
+                case 0:
+                    st[sp + codes[ip].a] = st[top--];
+                    continue;
+
+                case 1:
+                    st[codes[ip].a] = st[top--];
+                    continue;
+
+                case 2:
+                    st[sp + codes[ip].a] = st[top];
+                    continue;
+
+                case 3:
+                    st[codes[ip].a] = st[top];
+                    continue;
+
+                default:
+                    throw InstructionError("no such instruction", codes[ip].f.name); // TODO
+                }
+
+            case 0x4c4143: // CAL
+                if (codes[ip].l == 0)
+                {
+                    if ((top += 2) >= static_cast<int>(st.size()))
+                    {
+                        st.resize(top + 1);
+                    }
+                }
+                else
+                {
+                    if ((top += 3) >= static_cast<int>(st.size()))
+                    {
+                        st.resize(top + 1);
+                    }
+                }
+                st[top] = ip;
+                st[top - 1] = sp;
+                ip = codes[ip].a - 1;
+                sp = top - 1;
+                continue;
+
+            case 0x544e49: // INT
+                if ((top += codes[ip].a) >= static_cast<int>(st.size()))
+                {
+                    st.resize(top + 1);
+                }
+                continue;
+
+            case 0x504d4a: // JMP
+                ip = codes[ip].a - 1;
+                continue;
+
+            case 0x43504a: // JPC
+                if (!st[top--])
+                {
+                    ip = codes[ip].a - 1;
+                }
+                continue;
+
+            case 0x52504f: // OPR
+                switch (codes[ip].a)
+                {
+                case 0:
+                    top = sp - 1;
+                    ip = st[sp + 1];
+                    sp = st[sp];
+                    if (sp != 0)
+                    {
+                        continue;
+                    }
+                    return;
+
+                case 1:
+                    st[top] = -st[top];
+                    continue;
+
+                case 2:
+                    st[top - 1] += st[top];
+                    --top;
+                    continue;
+
+                case 3:
+                    st[top - 1] -= st[top];
+                    --top;
+                    continue;
+
+                case 4:
+                    st[top - 1] *= st[top];
+                    --top;
+                    continue;
+
+                case 5:
+                    st[top - 1] /= st[top];
+                    --top;
+                    continue;
+
+                case 6:
+                    st[top] &= 1;
+                    continue;
+
+                case 7:
+                    st[top] = !st[top];
+                    continue;
+
+                case 8:
+                    --top;
+                    st[top] = st[top] < st[top + 1];
+                    continue;
+
+                case 9:
+                    --top;
+                    st[top] = st[top] <= st[top + 1];
+                    continue;
+
+                case 10:
+                    --top;
+                    st[top] = st[top] > st[top + 1];
+                    continue;
+
+                case 11:
+                    --top;
+                    st[top] = st[top] >= st[top + 1];
+                    continue;
+
+                case 12:
+                    --top;
+                    st[top] = st[top] == st[top + 1];
+                    continue;
+
+                case 13:
+                    --top;
+                    st[top] = st[top] != st[top + 1];
+                    continue;
+
+                case 14:
+                    printf("%d\n", st[top--]); // TODO: Optimize
+                    fflush(stdout);
+                    continue;
+
+                case 15:
+                    puts("");
+                    fflush(stdout);
+                    continue;
+
+                case 16:
+                    if (++top >= static_cast<int>(st.size()))
+                    {
+                        st.resize(top + 1);
+                    }
+                    scanf("%d", st.data() + top);
+                    while (getchar() != '\n');
+                    continue;
+
+                case 17:
+                    if (++top >= static_cast<int>(st.size()))
+                    {
+                        st.resize(top + 1);
+                    }
+                    st[top] = getchar();
+                    while (getchar() != '\n');
+                    continue;
+
+                case 18:
+                    printf("%s", reinterpret_cast<char*>(st.data() + st[top--]));
+                    continue;
+
+                default:
+                    throw InstructionError("no such instruction", codes[ip].f.name); // TODO
+                };
+
+            case 0x41444c: // LDA
+                if (codes[ip].l == 0)
+                {
+                    st[top] = st[sp + codes[ip].a + st[top]];
+                    continue;
+                }
+                else
+                {
+                    st[top] = st[codes[ip].a + st[top]];
+                    continue;
+                }
+
+            case 0x415453: // STA
+                if (codes[ip].l == 0)
+                {
+                    st[sp + codes[ip].a + st[top - 1]] = st[top]; // TODO: Optimize
+                    top -= 2;
+                    continue;
+                }
+                else
+                {
+                    st[codes[ip].a + st[top - 1]] = st[top]; // TODO: Optimize
+                    top -= 2;
+                    continue;
+                }
+
+            default:
+                throw InstructionError("no such instruction", codes[ip].f.name); // TODO
+            }
+        }
+
     }
 
 } // namespace sci
